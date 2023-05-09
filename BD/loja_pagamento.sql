c6bca55d547ec5dd7a72cbccbde26cfd58983685
Celio Pires <celio.piresj@gmail.com>
2023-05-09 20:38:30 -0300

    BD

--- BD/loja_pagamento.sql
+++ BD/loja_pagamento.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `loja` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `loja`;
 -- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
 --
 -- Host: localhost    Database: loja
@@ -53,4 +55,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-05-06 21:26:29
+-- Dump completed on 2023-05-08 20:18:17
