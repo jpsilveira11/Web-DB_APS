4f95c24653b9261f35fce2da16b6b82f0dd5c3da
Celio Pires <celio.piresj@gmail.com>
2023-05-10 22:55:41 -0300

    atualizações

--- pixel-pay/src/components/container-header/index.tsx
+++ pixel-pay/src/components/container-header/index.tsx
@@ -4,7 +4,8 @@ function ContainerHeader(props: any){
     
     return(
         <>
-            <div className={`container backgroud-color-${props.color}`}>
+            <div className={`container backgroud-color-${props.color} ${props.className}`}>
+                 <span>{props.children}</span>
             </div>
         </>
     );
--- pixel-pay/src/pages/welcome/index.tsx
+++ pixel-pay/src/pages/welcome/index.tsx
@@ -1,12 +1,17 @@
 import "./index.css"
 import { MdOutlineAccountCircle } from "react-icons/md";
-import ContainerHeader from "../../components/container-header/index.tsx"
+import ContainerHeader from "../../components/container-header/index.tsx";
+import words from "../../assets/text-welcome.json"
 
 export default function Welcome(){
 
     return (
         <>
-             <ContainerHeader color="transparent"/>
+        
+             <ContainerHeader color="transparent" className="text-uppercase text-color-white" >
+                <div className="text-root-size">{words.header}</div>
+                <MdOutlineAccountCircle className="text-large"/>
+            </ContainerHeader>
 
             {/* <div className="button-container">
                 <h3>PIXEL PLAY</h3>
