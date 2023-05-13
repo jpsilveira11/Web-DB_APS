7ba10d964b9c57f193c5048634209e46a5fb3d84
Celio Pires <celio.piresj@gmail.com>
2023-05-13 09:28:17 -0300

    atualização

--- pixel-pay/src/components/card/index.tsx
+++ pixel-pay/src/components/card/index.tsx
@@ -13,7 +13,7 @@ function Card(_prop: {content: any}){
     return (
         <>
             <div className="card" style={{ width: '18rem' }}>
-               <img className="card-img-top" src={link} alt={content.nom} />  
+               <img className="card-img-top" src={link} alt={content.nome} />  
             </div>
         </>
     )
