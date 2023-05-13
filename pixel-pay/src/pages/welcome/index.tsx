53ea26b9ad445c56931b85f8db210dfa416d74fa
Celio Pires <celio.piresj@gmail.com>
2023-05-13 08:02:57 -0300

    atualizações

--- pixel-pay/src/pages/welcome/index.tsx
+++ pixel-pay/src/pages/welcome/index.tsx
@@ -1,27 +1,46 @@
 import "./index.css"
+import { useState, useEffect } from 'react';
 import { MdOutlineAccountCircle } from "react-icons/md";
 import ContainerHeader from "../../components/container-header/index.tsx";
 import words from "../../assets/text-welcome.json"
+import Card from "../../components/card/index.tsx";
+
+import axios from 'axios';
+
+function App() {
+
+    const [jogos, setJogos] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:5174/api/jogos/')
+        .then(response => {
+              setJogos(response.data);
+        })
+        .catch(error => {
+          console.error(error)
+        })
+    }, [])
+  
+    return (
+      <>
+       <Card content={jogos}></Card>
+      </>
+    )
+  }
+
 
 export default function Welcome(){
 
     return (
         <>
-        
-             <ContainerHeader color="transparent" className="text-uppercase text-color-white" >
+            <ContainerHeader color="transparent" className="text-uppercase text-color-white trigo" >
                 <div className="text-bold text-large">PIXEL PLAY</div>
                 <div className="nav">
                     <MdOutlineAccountCircle className="btn-icon"/>
                     <div className="btn-secondary">{words.header}</div>
-                </div>
-                
+                </div> 
             </ContainerHeader>
-
-            {/* <div className="button-container">
-                <h3>PIXEL PLAY</h3>
-                <button>LOJA</button>
-                <button> <MdOutlineAccountCircle/></button>
-            </div> */}
+           <App></App>
         </>   
     )
 
--- server/model.ts
+++ server/model.ts
@@ -12,13 +12,32 @@ app.listen(port, () => {
   console.log(`Servidor iniciado na porta ${port}`);
 });
 
-app.get('/api', (req, res) => {
-  connection.query('SELECT * FROM jogos', (error, results) => {
-    if (error) {
-      console.error(error);
-      res.status(500).send('Erro ao buscar dados');
-    } else {
-      res.json(results);
-    }
-  });
+app.get('/api/jogos', (req, res) => {
+
+  const jogo = req.query.JogoId || null;
+
+  if (!jogo){
+
+    connection.query('SELECT * FROM jogos', (error, results) => {
+      if (error) {
+        console.error(error);
+        res.status(500).send('Erro ao buscar dados');
+      } else {
+        res.json(results);
+      }
+    });
+
+  }
+  else { 
+    connection.query('SELECT * FROM jogos WHERE JogosId = ?', [jogo], (error, results) => {
+      if (error) {
+        console.error(error);
+        res.status(500).send('Erro ao buscar dados');
+      } else {
+        res.json(results);
+      }
+    });
+    
+  }
 });
+
